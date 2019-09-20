45134e25fc404ae895eb4c8fadfdf32b4b6fe6b5
Logon Aluno <logonaluno@local.com>
2019-09-20 11:41:26 -0300

    DAO E TESTE CONCLUIDA

---
+++ NacHumberto/src/br/com/techsow/principal/TesteUpdateEmail.java
@@ -0,0 +1,33 @@
+package br.com.techsow.principal;
+
+import br.com.techsow.beans.Usuario;
+import br.com.techsow.dao.UsuarioDAO;
+
+public class TesteUpdateEmail {
+
+	public static void main(String[] args) {
+		UsuarioDAO dao=null;
+		Usuario u=new Usuario();
+		try {
+			dao= new UsuarioDAO();
+			u.setId(1);
+			u.setNome("humb");
+			u.setEmail("email");
+			u.setSenha("senha");
+			dao.updateEmail(u, "email@email.com");
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				dao.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	}
+
+
