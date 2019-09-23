50db1650e56949d20eb6a48ee791208248eda156
Logon Aluno <logonaluno@local.com>
2019-09-23 09:46:59 -0300

    Adicionado DAO updateIdade

---
+++ NacHumberto/src/br/com/techsow/principal/TesteUpdateIdade.java
@@ -0,0 +1,41 @@
+package br.com.techsow.principal;
+
+import br.com.techsow.beans.Pessoa;
+import br.com.techsow.beans.Usuario;
+import br.com.techsow.dao.PessoaDAO;
+import br.com.techsow.dao.UsuarioDAO;
+
+public class TesteUpdateIdade {
+
+	public static void main(String[] args) {
+
+		PessoaDAO dao=null;
+		Pessoa p=new Pessoa();
+		
+		try {
+
+			dao= new PessoaDAO();
+			
+			p=new PessoaDAO().getPessoa(1);
+			
+
+			if(dao.updateIdade(p, 1)>=1) {
+				System.out.println("Idade alterada com sucesso");
+			}else {
+				System.out.println("Idade não alterada");
+			}
+		
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				dao.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
